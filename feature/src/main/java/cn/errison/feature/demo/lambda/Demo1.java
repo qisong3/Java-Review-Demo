e398b8e48e4b611af22f4678ae1070d552bcc3cb
Errison <qisong3@gmail.com>
2020-09-21 18:27:27 +0800

    更新JDK8示例。

---
+++ feature/src/main/java/cn/errison/feature/demo/lambda/Demo1.java
@@ -0,0 +1,60 @@
+package cn.errison.feature.demo.lambda;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.IntSupplier;
+
+public class Demo1 {
+
+    @FunctionalInterface
+    interface ConsumerInterface {
+
+        void testBiConsumer(int a, int b);
+    }
+
+    private static void compare() {
+        ConsumerInterface interface1 = new ConsumerInterface() {
+            @Override
+            public void testBiConsumer(int a, int b) {
+                System.out.println("传统定义方式");
+            }
+        };
+
+        ConsumerInterface interface2 = (a, b) -> System.out.println("Lambda语句");
+        interface1.testBiConsumer(1,2);
+        interface2.testBiConsumer(1,2);
+    }
+
+    private static void defineLambdaFunction() {
+        BiFunction<Integer, Long, String> biFunction = (a, b) -> "BiFunction return: a :" + a + "  b: " + b;
+        String str = biFunction.apply(1, Long.MAX_VALUE);
+        System.out.println(str);
+    }
+
+    private static void defineLambdaConsumer() {
+        BiConsumer<Integer, Long> biConsumer = (a, b) -> System.out.println("BiConsumer a :" + a + "  b: " + b);
+        biConsumer.accept(1, Long.MAX_VALUE);
+    }
+
+    private static void defineLambdaPredict() {
+        BiPredicate<Integer, Integer> biPredicate = (a, b) -> a > b;
+        System.out.println("Predict result : " + biPredicate.test(2, 4));;
+    }
+
+    private static void defineLambdaSupplier() {
+        IntSupplier supplier = () -> 3;
+        System.out.println("Supplier : " + supplier.getAsInt());
+    }
+
+
+    public static void main(String[] args) {
+
+        compare();
+        defineLambdaFunction();
+        defineLambdaConsumer();
+        defineLambdaPredict();
+        defineLambdaSupplier();
+
+    }
+}
